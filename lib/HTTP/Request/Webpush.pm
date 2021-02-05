0c1bb3224f7d703028d93d7826f77e00e9f96697
Erich Strelow Fiedler <esf@moller.cl>
2021-02-05 15:56:44 -0300

    Completando la parde POD

--- lib/HTTP/Request/Webpush.pm
+++ lib/HTTP/Request/Webpush.pm
@@ -3,7 +3,7 @@ package HTTP::Request::Webpush;
 use strict 'vars';
 use warnings;
 
-our$VERSION='0.01';
+our $VERSION='0.01';
 
 use base 'HTTP::Request';
 
@@ -16,14 +16,6 @@ use Crypt::PK::ECC 'ecc_shared_secret';
 use Digest::SHA 'hmac_sha256';
 use Carp;
 
-#References:
-#
-# Tutorials and code samples:
-#  https://developers.google.com/web/updates/2016/03/web-push-encryption
-#  https://developers.google.com/web/fundamentals/push-notifications/web-push-protocol
-#  https://adiary.adiary.jp/0391
-# Standards:
-#  https://tools.ietf.org/html/rfc8291
 
 #================================================================
 # hkdf()
@@ -104,8 +96,9 @@ sub subject($$) {
 sub encode($$) {
 
    my $self=shift();
-   my $enc=shift();
+   my $enc= shift() || 'aes128gcm';
 
+   
    #This method is inherited from HTTP::Message, but here only aes128gcm applies
    croak 'Only aes128gcm encoding available' unless ($enc eq 'aes128gcm');
 
@@ -113,8 +106,86 @@ sub encode($$) {
    croak 'Endpoint must be present for message encoding' unless ($self->url());
    croak 'Authentication keys must be present for message encoding' unless ($self->{'app-key'});
    croak 'UA auth params must be present for message encoding' unless ($self->{subscription}->{'keys'}->{'p256dh'} && $self->{subscription}->{'keys'}->{'auth'});
+   croak 'Message payload must be 3992 bytes or less' unless (length($self->content) <= 3992);
 
-   
+   #This is the JWT part
+   my $data={  
+     'aud' => $self->{subsription}->{endpoint},
+     'exp'=> time() + 86400  
+   };
+   $data->{'sub'}=$self->{'subject'} if ($self->{'subject'});
+ 
+   my $appk = Crypt::PK::ECC->new();
+   $appk->import_key_raw($self->{'app-key'},'secp256r1');
+   my $token = encode_jwt(payload => $data, key => $appk  , alg=>'ES256');
+   $self->header( 'Authorization' => "WebPush $token" );
+
+   #Now the encription step
+   my $salt=random_bytes(16);
+
+   my $pk; #This will be the session key for encryption
+   if ($self->{'ecc'}) {
+      $pk=$self->{'ecc'};
+   } else {
+      $pk->generate_key('prime256v1');
+   }
+
+   my $pub_signkey=$pk->export_key_raw('public');
+   my $sec_signkey=$pk->export_key_raw('private');
+
+   my $sk=Crypt::PK::ECC->new(); #This will be the UA endpoint key, which we know from the subscription object
+
+   #The p256dh key is given to us in X9.62 format. Crypt::PK::ECC should be able
+   #to read it as a "raw" format. But it's important to apply the base64url variant
+   my $ua_public=decode_base64url($self->{subscription}->{'keys'}->{'p256dh'});
+   $sk->import_key_raw($ua_public, 'secp256r1');
+
+   my $ecdh_secret=$pk->shared_secret($sk);
+   my $auth_secret= decode_base64url($self->{subscription}->{'keys'}->{'auth'});
+
+   #An earlier draft established this header string as Content-Encoding: auth
+   my $key_info='WebPush: info'.chr(0).$ua_public.$pub_signkey;
+   my $prk=$self->_hkdf($auth_secret, $ecdh_secret, $key_info,32);
+
+   #Again, an earlier RFC8291 draft used a different cek_info and nonce_info
+   my $cek_info='Content-Encoding: aes128gcm'.chr(0);
+   my $nonce_info='Content-Encoding: nonce'.chr(0);
+
+   my $cek=$self->_hkdf($salt,$prk,$cek_info,16);
+   my $nonce= $self->_hkdf($salt, $prk,$nonce_info,12);
+
+   #Now we have all the ingredients, it's time for encryption
+   my ($body, $tag) = gcm_encrypt_authenticate('AES', $cek, $nonce, '', $self->content."\x02\x00");
+
+   #Some final composition
+   #0x00001000" is the coding unit of AES-GCM, and x41 is the key length
+   $body = $salt."\x00\x00\x10\x00\x41".$pub_signkey.$body.$tag;
+
+   $self->content($body);
+   $self->remove_header('Content-Length', 'Content-MD5','Content-Encoding','Content-Type','Encryption','Crypto-Key');
+   $self->header('Encryption' => "salt=".encode_base64url($salt));
+   $self->header('Crypto-Key' => "p256ecdsa=". encode_base64url($self->{'app-pub'}));
+   $self->header('Content-Length' => length($body));
+   $self->header('Content-Type' => 'application/octet-stream');
+   $self->header('Content-Encoding' => 'aes128gcm');
+
+   return 1;
+}
+
+sub decoded_content
+{
+    my($self, %opt) = @_;
+
+    #This is included so will always fail. decoded_content is inherited from HTTP::Message
+    croak 'No decoding available';
+
+}
+
+sub decode {
+    my($self, %opt) = @_;
+    
+    #This is included so will always fail. decode is inherited from HTTP::Message
+    croak 'No decoding available';
 }
 
 sub new($%) {
@@ -126,7 +197,7 @@ sub new($%) {
 
    bless $class, $self;
 
-   my @Options= ('auth','subscription','authbase64','reuseecc','subject');
+   my @Options= ('auth','subscription','authbase64','reuseecc','subject','content');
    for (@Options) {
       &$_($self,$opts{$_}) if (exists $opts{$_});
    }
@@ -150,27 +221,68 @@ version 0.01
 
 =head1 SYNOPSIS
 
-  some here
+ use HTTP::Request::Webpush;
 
+ #This should be the application wide key
+ #The APP_PUB part must be the same used by the user UA when requesting the subscription
+ use constant APP_PUB => 'BCAI00zPAbxEVU5w8D1kZXVs2Ro--FmpQNMOd0S0w1_5naTLZTGTYNqIt7d97c2mUDstAWOCXkNKecqgS4jARA8';
+ use constant APP_KEY => 'M6xy5prDBhJNlOGnOkMekyAQnQSWKuJj1cD06SUQTow';
+ 
+ #This should be previously collected from an already subscribed user UA
+ my $subscription='{"endpoint":"https://foo/fooer","expirationTime":null,"keys":{"p256dh":"BCNS...","auth":"dZ..."}}';
 
+ my $message=HTTP::Request::Webpush->new();
+ $message->auth(APP_PUB, APP_KEY);
+ $message->subscription($subscription);
+ $message->subject('mailto:bobsbeverage@some.com');
+ $message->content('Come taste our new lager brand');
+ 
+ #Additional headers can be applied with inherited HTTP::Response methods
+ $message->header('TTL' => '90');
+
+ #To send a single push message
+ $message->encode();
+ my $ua = LWP::UserAgent->new;
+ my $response = $ua->request($message);
+
+ #To send a batch of messages using the same application's end encryption key
+ my $ecc = Crypt::PK::ECC->new();
+ $ecc->generate_key('prime256v1');
+ 
+ for (@cleverly_stored_subcriptions) {
+    my $message=HTTP::Request::Webpush->new(reuseecc => $ecc);
+    $message->subscription($_);
+    $message->subject('mailto:bobsbeverage@some.com');
+    $message->content('Come taste our new pale ale brand');
+    $message->encode();
+    my $response = $ua->request($message);
+ }
+ 
 =head1 DESCRIPTION
 
 C<HTTP::Request::Webpush> produces an HTTP::Request for Application-side Webpush
-notifications as described on RFC8291. In this scheme, an Application is a 
+notifications as described on L<RFC8291|https://tools.ietf.org/html/rfc8291>.
+In this scheme, an Application is a 
 server-side component that sends push notification to previously subscribed
-browser workers. This class only covers the Application role. A lot must 
+browser worker(s). This class only covers the Application role. A lot must 
 be done on the browser side to setup a full working push notification system.
 
-In practical terms, this class is a glue for all the encription steps involved
-in setting up a RFC8291 message, along with the RFC8292 VAPID scheme.
+In practical terms, this class is a glue for all the encryption steps involved
+in setting up a RFC8291 message, along with the <RFC8292|https://tools.ietf.org/html/rfc8291> VAPID scheme.
 
 =over 4
+
+=item $r=HTTP::Request::Webpush->new()
+
+=item $r=HTTP::Request::Webpush-new(auth => $my_key, subscription => $my_subs, content='New lager batch arrived')
+
+The following options can be supplied in the constructor: subscription, auth, reuseecc, subject, content.
 
 =item $r->subscription($hash_reference)
 
 =item $r->subscription('{"endpoint":"https://foo/fooer","expirationTime":null,"keys":{"p256dh":"BCNS...","auth":"dZ..."}}');
 
-This sets the subcription object related to this notification service. This should be the same object
+This sets the subscription object related to this notification service. This should be the same object
 returned inside the browser environment using the PushManager.subscribe() method. The argument can
 be either a JSON string or a previously setup hash reference.
 
@@ -180,7 +292,7 @@ be either a JSON string or a previously setup hash reference.
 
 =item $r->authbase64('BCAI00zPAbxEVU5w8D1kZXVs2Ro--FmpQNMOd0S0w1_5naTLZTGTYNqIt7d97c2mUDstAWOCXkNKecqgS4jARA8','M6xy5prDBhJNlOGnOkMekyAQnQSWKuJj1cD06SUQTow')
 
-This sets the authentification key for the VAPID authentication scheme related to the push service.
+This sets the authentication key for the VAPID authentication scheme related to this push service.
 This can either be a (public, private) pair or an already setup Crypt::PK::ECC object. The public part
 must be the same used earlier in the browser environment in the PushManager.subscribe() applicationServerKey option.
 The key pair can be passed as URL safe base64 strings using the authbase64() variant.
@@ -198,7 +310,47 @@ isn't enforced since RFC8292 mentions this as a SHOULD practice. But, if a valid
 is not included, the browser push service is likely to bounce the message. The URI passed is 
 used as the 'sub' claim in the authentication JWT.
 
+=item $r->content('Try our new draft beer')
+
+This sets the unencripted message content, or the payload in terms of rfc8291.
+This is actually inherited from L<HTTP::Message>,
+as well as other methods that can be used to set the message content.
+
+=item $r->encode('aes128gcm')
+
+This does the encryption process, as well as setting the headers expected by the push service.
+aes128gcm is the only acceptable argument. Before calling this, the subscription and auth must
+be supplied. You must call this method before submitting the message, otherwise the encryption
+process won't happen.
+
+Please note that encode() and content() are inherited from L<HTTP::Message>. No backward decryption
+is provided by this class, so the decoded_content() and decode() methods will fail. After you
+encode(), the content can still be accessed through HTTP::Message standard methods and it
+will be the binary body of the encrypted message.
+
 =back
+
+=head1 REFERENCES
+
+This class relies on L<Digest::SHA> for the HKDF derivation, 
+L<Crypt::AuthEnc::GCM> for the encryption intself, L<Crypt::PK::ECC> for key management and
+L<Crypt::PRNG> for the salt.
+
+RFC8291 establish the encription steps: L<https://tools.ietf.org/html/rfc8291>
+
+RFC8291 establish the VAPID scheme: L<https://tools.ietf.org/html/rfc8292>
+
+The following code samples and tutorials were very useful:
+
+L<https://developers.google.com/web/updates/2016/03/web-push-encryption>
+
+L<https://developers.google.com/web/fundamentals/push-notifications/web-push-protocol>
+
+L<https://adiary.adiary.jp/0391>
+
+=head1 AUTHOR
+
+Erich Strelow <estrelow@cpan.org>
 
 =cut
 
